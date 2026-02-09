a833b2a48d6397754ccf81363acd70f264d99ea0
darmuh <darmuh8@gmail.com>
2026-02-08 23:45:54 -0800

    Add project files.

---
+++ Plumber.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using Mirror;
+using Mirror.RemoteCalls;
+
+namespace MirrorPlumber;
+
+public delegate void NetworkEvent();
+public delegate void NetworkEvent<T>(T param);
+public delegate void NetworkEvent<T1, T2>(T1 param1, T2 param2);
+
+/// <summary>
+/// This is the plumber base that all Plumber types use
+/// </summary>
+/// <remarks>
+/// You should not ever be creating a PlumberBase on it's own. It will be created when creating your specificly typed Plumber instance.
+/// </remarks>
+public abstract class PlumberBase
+{
+    internal Type type;
+    internal readonly string _fullName;
+    internal readonly ushort _hash;
+    internal readonly bool _requiresAuth = false;
+    internal bool _includeOwner = false;
+    internal RemoteCallType _callType;
+    internal readonly NetType _netType;
+
+    /// <summary>This determines the type of Mirror Remote Action you are requesting plumbing for.</summary>
+    /// <remarks>
+    /// For more information on the different types, see https://github.com/MirrorNetworking/MirrorDocs/blob/main/manual/guides/communications/remote-actions.md
+    /// </remarks>
+    public enum NetType
+    {
+        Command,
+        ClientRpc,
+        TargetRpc
+    }
+
+    //Base constructor for classes that inherit this one to assign all of the necessary values
+    internal PlumberBase(Type netBehaviour, string methodName, NetType netType, bool requiresAuthority = false)
+    {
+        type = netBehaviour;
+        _fullName = GetFullName(methodName);
+        _hash = GetHash(_fullName);
+        _requiresAuth = requiresAuthority;
+        _netType = netType;
+    }
+
+    //This method should be called on the constructor method for classes that inherit this one
+    internal void Register()
+    {
+        if (_netType == NetType.Command)
+            _callType = RemoteCallType.Command;
+        else
+            _callType = RemoteCallType.ClientRpc;
+
+        if (_netType == NetType.Command)
+            RemoteProcedureCalls.RegisterDelegate(type, _fullName, _callType, StandardCommand, _requiresAuth);
+        else if (_netType == NetType.ClientRpc)
+            RemoteProcedureCalls.RegisterDelegate(type, _fullName, _callType, StandardRPC, _requiresAuth);
+        else if (_netType == NetType.TargetRpc)
+            RemoteProcedureCalls.RegisterDelegate(type, _fullName, _callType, StandardRPC, _requiresAuth);
+    }
+
+    internal abstract void StandardCommand(NetworkBehaviour obj, NetworkReader reader, NetworkConnectionToClient senderConnection);
+    internal abstract void StandardRPC(NetworkBehaviour obj, NetworkReader reader, NetworkConnectionToClient senderConnection);
+
+    // Utility method for packing and sending the remote action based on what type of action it is 
+    internal void SendToMirror<T>(T instance, NetworkWriterPooled writer, NetworkConnection target = null!) where T : NetworkBehaviour
+    {
+        if (_netType == NetType.Command)
+            instance.SendCommandInternal(_fullName, _hash, writer, 0, _requiresAuth);
+        else if (_netType == NetType.ClientRpc)
+            instance.SendRPCInternal(_fullName, _hash, writer, 0, _includeOwner);
+        else if (_netType == NetType.TargetRpc && target != null)
+            instance.SendTargetRPCInternal(target, _fullName, _hash, writer, 0);
+        else
+        {
+            Plugin.Log.LogWarning($"{_fullName}: TargetRpc Invoke missing target parameter!");
+            return;
+        }
+
+        NetworkWriterPool.Return(writer);
+    }
+
+    // Used as part of registering the network action with mirror, this emulates Mirror's standard registration
+    private string GetFullName(string methodName)
+    {
+        return $"{type.GetMethod(methodName).ReturnType} {type.Namespace}::{methodName}";
+    }
+
+    // Used as part of registering the network action with mirror, this emulates Mirror's standard registration
+    private static ushort GetHash(string fullName)
+    {
+        return (ushort)(fullName.GetStableHashCode() & 0xFFFF);
+    }
+
+    /// <summary>
+    /// Set includeOwner property for sending Rpcs.
+    /// </summary>
+    /// <param name="includeOwner"></param>
+    /// <remarks>Owner refers to the network object's owner, not the sender of the ClientRpc</remarks>
+    public void SetRpcIncludeOwner(bool includeOwner)
+    {
+        _includeOwner = includeOwner;
+    }
+}
+
+/// <summary>
+/// This is your standard plumber for any network action that does not use any parameters
+/// </summary>
+/// <remarks>
+/// You should define your Plumber for your NetworkBehaviour type at Awake. As it will be registered as soon as it is defined.
+/// If you do not define the Plumber in Awake, make sure you define it before you try to run any NetworkAction methods.
+/// </remarks>
+public class Plumber : PlumberBase
+{
+    /// <summary>
+    /// This constructor method will immediately register your Plumber with Mirror. It's recommended to do this at your NetworkBehaviour's Awake method.
+    /// </summary>
+    /// <param name="netBehaviour">The System.Type converison of your NetworkBehaviour. ie. typeof(MyNetworkBehaviour)</param>
+    /// <param name="methodName">This is the name of the method you wish to perform plumbing for. ie. nameof(MyNetworkedMethod)</param>
+    /// <param name="netType">This is the type of Mirror Network Action you wish to perform plumbing for.</param>
+    /// <param name="requiresAuthority">If enabled, only the host client can invoke this network action</param>
+    public Plumber(Type netBehaviour, string methodName, NetType netType, bool requiresAuthority = false) : base(netBehaviour, methodName, netType, requiresAuthority)
+    {
+        Register();
+    }
+
+    private event NetworkEvent NetworkedEvent = null!;
+
+    /// <summary>
+    /// Add a method as a listener to this Network Action.
+    /// </summary>
+    /// <param name="listener">The method you wish to run when your Network Action is called</param>
+    public void AddListener(NetworkEvent listener)
+    {
+        NetworkedEvent += listener;
+    }
+
+    /// <summary>
+    /// This Method handles the rest of Mirror's plumbing for you and invokes the NetworkedEvent your methods are listening to.
+    /// </summary>
+    /// <param name="instance">The instance of your NetworkBehaviour Type that is calling this method</param>
+    /// <param name="target">If this NetworkAction is a TargetRpc, specify the target of the Rpc here</param>
+    public void Invoke<TSource>(TSource instance, NetworkConnection target = null!) where TSource : NetworkBehaviour
+    {
+        NetworkWriterPooled writer = NetworkWriterPool.Get();
+        SendToMirror(instance, writer, target);
+    }
+
+    internal override void StandardRPC(NetworkBehaviour obj, NetworkReader reader, NetworkConnectionToClient senderConnection)
+    {
+        if (!NetworkClient.active)
+        {
+            Plugin.Log.LogError("NetworkingTestWithPlumbing - RPC RpcShowHelloMessage called on server.");
+        }
+        else
+        {
+            NetworkedEvent?.Invoke();
+        }
+    }
+
+    internal override void StandardCommand(NetworkBehaviour obj, NetworkReader reader, NetworkConnectionToClient senderConnection)
+    {
+        if (!NetworkServer.active)
+        {
+            Plugin.Log.LogError("NetworkServer is not active, no clients to send this to");
+        }
+        else
+        {
+            NetworkedEvent?.Invoke();
+        }
+    }
+}
+
+/// <summary>
+/// This is a Plumber for any Network Action that takes one parameter
+/// </summary>
+/// <typeparam name="TParam">This is the type your network action takes as a parameter.</typeparam>
+/// <remarks>
+/// TParam can be any type that Mirror can successfully read/write with. For more information see https://github.com/MirrorNetworking/MirrorDocs/blob/main/manual/guides/data-types.md
+/// You should define your Plumber for your NetworkBehaviour type at Awake. As it will be registered as soon as it is defined.
+/// If you do not define the Plumber in Awake, make sure you define it before you try to run any NetworkAction methods.
+/// </remarks>
+public class Plumber<TParam> : PlumberBase
+{
+    /// <summary>
+    /// This constructor method will immediately register your Plumber with Mirror. It's recommended to do this at your NetworkBehaviour's Awake method.
+    /// </summary>
+    /// <param name="netBehaviour">The System.Type converison of your NetworkBehaviour. ie. typeof(MyNetworkBehaviour)</param>
+    /// <param name="methodName">This is the name of the method you wish to perform plumbing for. ie. nameof(MyNetworkedMethod)</param>
+    /// <param name="netType">This is the type of Mirror Network Action you wish to perform plumbing for.</param>
+    /// <param name="requiresAuthority">If enabled, only the host client can invoke this network action</param>
+    public Plumber(Type netBehaviour, string methodName, NetType netType, bool requiresAuthority = false) : base(netBehaviour, methodName, netType, requiresAuthority)
+    {
+        Register();
+    }
+
+    private event NetworkEvent<TParam> NetworkedEvent = null!;
+
+    /// <summary>
+    /// Add a method as a listener to this Network Action.
+    /// </summary>
+    /// <param name="listener">The method you wish to run when your Network Action is called. It must take a parameter of the same type as TParam</param>
+    public void AddListener(NetworkEvent<TParam> listener)
+    {
+        NetworkedEvent += listener;
+    }
+
+    /// <summary>
+    /// This Method handles the rest of Mirror's plumbing for you and invokes the NetworkedEvent your methods are listening to.
+    /// </summary>
+    /// <typeparam name="TSource">This is your NetworkBehaviour Type which this is most likely being called from</typeparam>
+    /// <param name="instance">The instance of your NetworkBehaviour Type that is calling this method</param>
+    /// <param name="param">The value of type TParam you are sending over the network</param>
+    /// <param name="target">If this NetworkAction is a TargetRpc, specify the target of the Rpc here</param>
+    public void Invoke<TSource>(TSource instance, TParam param, NetworkConnection target = null!) where TSource : NetworkBehaviour
+    {
+        NetworkWriterPooled writer = NetworkWriterPool.Get();
+
+        //give mirror writer the param
+        writer.Write(param);
+
+        SendToMirror(instance, writer, target);
+    }
+
+    internal override void StandardRPC(NetworkBehaviour obj, NetworkReader reader, NetworkConnectionToClient senderConnection)
+    {
+        if (!NetworkClient.active)
+        {
+            Plugin.Log.LogError("NetworkingTestWithPlumbing - RPC RpcShowHelloMessage called on server.");
+        }
+        else
+        {
+            NetworkedEvent?.Invoke(reader.Read<TParam>());
+        }
+    }
+
+    internal override void StandardCommand(NetworkBehaviour obj, NetworkReader reader, NetworkConnectionToClient senderConnection)
+    {
+        if (!NetworkServer.active)
+        {
+            Plugin.Log.LogError("NetworkServer is not active, no clients to send this to");
+        }
+        else
+        {
+            NetworkedEvent?.Invoke(reader.Read<TParam>());
+        }
+    }
+}
+
+/// <summary>
+/// This is a Plumber for any Network Action that takes two parameters
+/// </summary>
+/// <typeparam name="TParam1">This is the first type your network action takes as a parameter</typeparam>
+/// <typeparam name="TParam2">This is the second type your network action takes as a parameter</typeparam>
+/// <remarks>
+/// TParam1 & TParam2 can be any type that Mirror can successfully read/write with. For more information see https://github.com/MirrorNetworking/MirrorDocs/blob/main/manual/guides/data-types.md
+/// You should define your Plumber for your NetworkBehaviour type at Awake. As it will be registered as soon as it is defined.
+/// If you do not define the Plumber in Awake, make sure you define it before you try to run any NetworkAction methods.
+/// </remarks>
+public class Plumber<TParam1, TParam2> : PlumberBase
+{
+    /// <summary>
+    /// This constructor method will immediately register your Plumber with Mirror. It's recommended to do this at your NetworkBehaviour's Awake method.
+    /// </summary>
+    /// <param name="netBehaviour">The System.Type converison of your NetworkBehaviour. ie. typeof(MyNetworkBehaviour)</param>
+    /// <param name="methodName">This is the name of the method you wish to perform plumbing for. ie. nameof(MyNetworkedMethod)</param>
+    /// <param name="netType">This is the type of Mirror Network Action you wish to perform plumbing for.</param>
+    /// <param name="requiresAuthority">If enabled, only the host client can invoke this network action</param>
+    public Plumber(Type netBehaviour, string methodName, NetType netType, bool requiresAuthority = false) : base(netBehaviour, methodName, netType, requiresAuthority)
+    {
+        Register();
+    }
+
+    private event NetworkEvent<TParam1, TParam2> NetworkedEvent = null!;
+
+    /// <summary>
+    /// Add a method as a listener to this Network Action.
+    /// </summary>
+    /// <param name="listener">The method you wish to run when your Network Action is called. It must take two parameters with the types TParam1 and TParam2 (in that order)</param>
+    public void AddListener(NetworkEvent<TParam1, TParam2> listener)
+    {
+        NetworkedEvent += listener;
+    }
+
+    /// <summary>
+    /// This Method handles the rest of Mirror's plumbing for you and invokes the NetworkedEvent your methods are listening to.
+    /// </summary>
+    /// <typeparam name="TSource">This is your NetworkBehaviour Type which this is most likely being called from</typeparam>
+    /// <param name="instance">The instance of your NetworkBehaviour Type that is calling this method</param>
+    /// <param name="param1">The value of type TParam1 you are sending over the network</param>
+    /// <param name="param2">The value of type TParam2 you are sending over the network</param>
+    /// <param name="target">If this NetworkAction is a TargetRpc, specify the target of the Rpc here</param>
+    public void Invoke<TSource>(TSource instance, TParam1 param1, TParam2 param2, NetworkConnection target = null!) where TSource : NetworkBehaviour
+    {
+        NetworkWriterPooled writer = NetworkWriterPool.Get();
+
+        //give mirror writer the params
+        writer.Write(param1);
+        writer.Write(param2);
+
+        SendToMirror(instance, writer, target);
+    }
+
+    internal override void StandardRPC(NetworkBehaviour obj, NetworkReader reader, NetworkConnectionToClient senderConnection)
+    {
+        if (!NetworkClient.active)
+        {
+            Plugin.Log.LogError("NetworkingTestWithPlumbing - RPC RpcShowHelloMessage called on server.");
+        }
+        else
+        {
+            NetworkedEvent?.Invoke(reader.Read<TParam1>(), reader.Read<TParam2>());
+        }
+    }
+
+    internal override void StandardCommand(NetworkBehaviour obj, NetworkReader reader, NetworkConnectionToClient senderConnection)
+    {
+        if (!NetworkServer.active)
+        {
+            Plugin.Log.LogError("NetworkServer is not active, no clients to send this to");
+        }
+        else
+        {
+            NetworkedEvent?.Invoke(reader.Read<TParam1>(), reader.Read<TParam2>());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,122 @@
+# Mirror Plumber
+
+**Utility BepInEx Mod that allows modders to insert and use new NetworkActions (Commands, ClientRpcs, and TargetRpcs)**  
+
+- This was built for YAPYAP, however, it should work in any other unity game that utilizes the same style of Mirror Networking.  
+
+## Mirror Network Prefabs 
+ *Note: These steps assume you have some prior Unity modding knowledge.*  
+ *If you do not need a background on Mirror Networking and just wnat to know how to use this utility, skip to ``Using MirrorPlumber for your NetworkBehaviour``*
+
+### Create a GameObject that will serve as your Network Prefab containg your NetworkBehaviour  
+ - This game object should at the minimum contain a ``Networkidentity`` component.  
+	- Your class inheriting NetworkBehaviour should also be attached to this game object either during in Unity or added during runtime.  
+	- You cannot add a ``Networkidentity`` component at runtime because the assetId property will not be set properly.  
+ - Once you have created your Prefab with the ``NetworkIdentity`` component, build it into an asset bundle.  
+
+### Loading your GameObject Assetbundle as a Network Prefab  
+ - Load your asset bundle containing your game object at Plugin Awake. If you have not added your Network Behaviour classes already, add them as components now.  
+	- You'll want to cache your GameObject as it will be used in a few other places.  
+ - Either at Plugin Start or at another appropriate time (before hosting a game session), you'll want to then use ``NetworkClient.RegisterPrefab`` on your game object.  
+	- You'll also need to cache the assetId by performing ``GetComponent<NetworkIdentity>().assetId`` after you've registered your prefab.  
+    
+### Spawning your Network Prefab  
+ - Once your prefab has been properly registered, you can now spawn it once a game session has been started and the server is active.  
+	- Ensure you are ONLY spawning the object from the server client.  
+ - To spawn a NetworkPrefab you will Instantiate a copy of your cached GameObject and then run ``NetworkServer.Spawn`` on the copy with your cached assetId.  
+
+### YAPYAP Simple NetworkPrefab Load/Spawn Example
+
+```
+        internal static ManualLogSource Log { get; private set; } = null!;
+        internal static GameObject Networker = null!;
+        internal static uint NetworkerID = default!;
+
+        private void Awake()
+        {
+            Log = Logger;
+
+            Log.LogInfo($"Plugin {Name} is loaded!");
+
+            //Networker Bundle
+            string networkAsset = Path.Combine(Path.GetDirectoryName(Info.Location), "networker");
+            AssetBundle networker = AssetBundle.LoadFromFile(networkAsset);
+            Networker = networker.LoadAsset<GameObject>("Networker");
+            Networker.AddComponent<NetworkingTestWithPlumbing>();
+            GameManager.OnPlayerSpawned += OnPawnSpawn;
+        }
+
+        private void Start()
+        {
+            Log.LogMessage("Registering network prefabs");
+            NetworkClient.RegisterPrefab(Networker);
+            NetworkerID = Networker.GetComponent<NetworkIdentity>().assetId;
+            Log.LogDebug($"NetworkerID - {NetworkerID}");
+        }
+
+        private static void OnPawnSpawn(Pawn pawn)
+        {
+            Log.LogMessage($"Pawn spawned {pawn.PlayerName}");
+
+            if (!pawn.isLocalPlayer)
+                return;
+
+            if (pawn.isServer)
+            {
+                Log.LogDebug("Spawning networker");
+                var networker = Object.Instantiate(Networker);
+                NetworkServer.Spawn(networker, NetworkerID);
+            }
+        }
+```
+
+**In the above example:**
+ - The NetworkBehaviour ``NetworkingTestWithPlumbing`` is added to the Networker GameObject at Plugin Awake  
+ - Networker is then registered as a Network Prefab in Plugin Start and the assetId is cached as NetworkerID
+ - OnPlayerSpawn is listening to the GameManager.OnPlayerSpawned event. When a player is spawned, it checks if the player is the server and then also spawns the testing networker object.
+
+### Where MirrorPlumber comes in
+ - Without MirrorPlumber, this is where you would find that the various Attributes ``[ClientRpc]``, ``[Command]``, etc. are not actually utilizing Mirror to send the information across the network.  
+ - You *could* perform your own plumbing and register each and every NetworkAction yourself following the logic for ``RemoteProcedureCalls.RegisterDelegate`` or ``RemoteProcedureCalls.RegisterRpc`` or `RemoteProcedureCalls.RegisterCommand`  
+    - However this process is incredibly tedious and will certainly cause you some headaches.  
+ - MirrorPlumber does all of this manual plumbing for you. You'll just need to perform a few alternative steps in your NetworkBehaviour to start the process.  
+ 
+ ## Using MirrorPlumber for your NetworkBehaviour
+ - To start, it is recommended to create a static reference to each Plumber you create.
+    - This is because you will be the one to invoke these network actions in your code, so you'll need a reference to it.
+    - Example: 
+ ```
+    internal static Plumber GeneratedCommand = null!;
+    internal static Plumber<string, int> GeneratedRPC = null!;
+ ```
+
+ - Creating the Plumbers is recommended to be done in your NetworkBehaviour's Awake method, however it can be done any time after the prefab holding your NetworkBehaviour has been spawned.  
+    - You create the Plumber with a simple constructor that takes the following parameters:
+        - ``System.Type netBehaviour`` You'll provide this by converting your NetworkBehaviour to a System.Type - ie: ``typeof(MyNetworkBehaviour)``
+        - ``string methodName`` This is the name of the Method you are performing plumbing for - ie: ``nameof(MyNetworkedMethod)``
+        - ``NetType netType`` This is an enum created by MirrorPlumber to help identify what kind of NetworkAction you are requesting plumbing for.
+        - ``bool requiresAuthority = false`` (Optional) This bool determines if the NetworkAction requires ownership of the Network Prefab before being used. 
+    - NOTE: You cannot invoke Network Actions without having created your Plumber.
+    - Once you have created your Plumber, you should then add the method you wish to run when the NetworkAction is received via ``AddListener``. 
+      - This can be multiple methods so long as each one has the number of parameters expected with the correct type.
+- Example: 
+ ``` 
+    private void Awake()
+    {
+        GeneratedCommand = new(typeof(NetworkingTestWithPlumbing), nameof(CmdSendHello), PlumberBase.NetType.Command);
+        GeneratedCommand.AddListener(RpcShowHelloMessage);
+
+        GeneratedRPC = new(typeof(NetworkingTestWithPlumbing), nameof(RpcShowHelloMessage), PlumberBase.NetType.TargetRpc);
+        GeneratedRPC.AddListener(HelloFromTheNetwork);
+    }
+ ```
+ - Now that you have your Plumbers created, all you need to do now is Invoke them wherever you would typically call the method with the NetworkAction.
+    - It's recommended to use a [null conditional operator](https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/member-access-operators) before invoking your Plumber.
+    - Below are some of the parameters you can expect in the Plumber's Invoke method:
+        - ``TSource instance`` This is the instance of your NetworkBehaviour that is calling this method.
+        - ``NetworkConnection target = null!`` (OPTIONAL) If your NetworkAction is a TargetRpc, you'll need to set this to the client you wish to send the NetworkAction to.
+        - ``TParam param`` or ``TParam1 param1`` or ``TParam2 param2`` All of these potential parameters are values of the parameter types you are sending to other clients.
+    - Examples: ``GeneratedCommand?.Invoke(this);`` ``GeneratedRPC?.Invoke(this, "Hello World from the network!", 1337, NetworkServer.connections[0]);``
+        - Both of these examples are called from inside non-static methods inside my NetworkBehaviour ``NetworkingTestWithPlumbing``
+    - For information on what types can be passed in TParams over to Mirror's NetworkWriter/NetworkReader, please see [this page](https://github.com/MirrorNetworking/MirrorDocs/blob/main/manual/guides/data-types.md) of Mirror's Documentation.
+        - Some games may also have custom NetworkWriter/NetworkReaders that you can utilize, YAPYAP does not and you cannot add new ones via a Mod (they require Weaving from Mirror)
